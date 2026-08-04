a57a3bfa9756b66d26e17ca0cf1d4eedd831afb9
Ben May <maybenjamin93@gmail.com>
2026-08-04 17:07:03 -0400

    added read me and reorganized project folders

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project Title
+
+Brazil E-commerce Data Analysis
+
+# Dataset Used
+
+https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce
+
+## Installation
+
+git clone https://github.com/maybenjamin04/retail-sales-analytics.git
+cd retail-sales-analytics
+
+## Usage
+
+Download Dataset and move into data
+
+cd python
+python3 dataCleaning.py -> cleans data and stores into new csv files
+python3 loadToPosgreSQL.py -> loads cleaned data into PostgreSQL database and validates data
+
+cd sql
+create_tables.sql -> holds structure for database and creates db tables
+basic_qureies.sql -> basic queries used for debug and basic data analysis
+analytical_queries -> used to create custom tables for tableau and used for data insights
--- sql/analytical_queries.sql
+++ sql/analytical_queries.sql
@@ -39,6 +39,7 @@ FROM orders;
 --Seller Performance
 SELECT t1.id,
        t5.state_code,
+       t6.category_name_english,
        COUNT(t2.product_id) as num_products_sold,
        ROUND(SUM(t2.price::numeric + COALESCE(t2.freight_value::numeric, 0)), 2) as revenue,
        ROUND(AVG(t4.score), 2) as avg_review_score,
@@ -46,10 +47,11 @@ SELECT t1.id,
 FROM sellers t1
     INNER JOIN order_items t2 ON t1.id = t2.seller_id
     INNER JOIN orders t3 ON t2.order_id = t3.id
+    INNER JOIN products t6 ON t2.product_id = t6.id
     LEFT JOIN reviews t4 ON t3.id = t4.order_id
     INNER JOIN geolocation t5 ON t1.zip_code_prefix = t5.zip_code_prefix
-GROUP BY t1.id, t5.state_code
-ORDER BY revenue DESC;
+GROUP BY t1.id, t5.state_code, t6.category_name_english
+ORDER BY t1.id, revenue DESC;
 
 --Date Revenue
 SELECT 
